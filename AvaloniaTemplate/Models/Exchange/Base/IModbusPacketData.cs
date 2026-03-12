3c847f2dae13748fcc6fa9d917719a8b867ceab6
Pakhomov Nikolay <pakhomovnikolay@gmail.com>
2026-03-12 17:27:22 +0300

    Создана модель для формирования данных Modbus пакета

---
+++ AvaloniaTemplate/Models/Exchange/Base/IModbusPacketData.cs
@@ -0,0 +1,26 @@
+﻿namespace AvaloniaTemplate.Models.Exchange.Base
+{
+    public interface IModbusPacketData
+    {
+        #region Стартовый адрес пакета
+        /// <summary>
+        /// Стартовый адрес пакета
+        /// </summary>
+        ushort StartAddress { get; set; }
+        #endregion
+
+        #region Буфер
+        /// <summary>
+        /// Буфер
+        /// </summary>
+        ushort[] Data { get; set; }
+        #endregion
+
+        #region Размер буфера
+        /// <summary>
+        /// Размер буфера
+        /// </summary>
+        int Length { get; }
+        #endregion
+    }
+}
---
+++ AvaloniaTemplate/Models/Exchange/ModbusPacketData.cs
@@ -0,0 +1,28 @@
+﻿using AvaloniaTemplate.Models.Exchange.Base;
+
+namespace AvaloniaTemplate.Models.Exchange
+{
+    public class ModbusPacketData : IModbusPacketData
+    {
+        #region Стартовый адрес пакета
+        /// <summary>
+        /// Стартовый адрес пакета
+        /// </summary>
+        public ushort StartAddress { get; set; }
+        #endregion
+
+        #region Буфер
+        /// <summary>
+        /// Буфер
+        /// </summary>
+        public ushort[] Data { get; set; }
+        #endregion
+
+        #region Размер буфера
+        /// <summary>
+        /// Размер буфера
+        /// </summary>
+        public int Length { get => Data.Length; }
+        #endregion
+    }
+}
