98fc5632315348a497188260c87ce1c4892959cd
Николай <pakhomovnikolay@live.com>
2026-06-19 11:29:57 +0300

    В CustomResourcesDictionary добавлена кнопка ButtonCopy

---
+++ AvaloniaTemplate/Infrastructures/Commands/UserCommand/Command_Copy.cs
@@ -0,0 +1,20 @@
+﻿using AvaloniaTemplate.Infrastructures.Commands.Base;
+using AvaloniaTemplate.Services.Interfaces;
+
+namespace AvaloniaTemplate.Infrastructures.Commands.UserCommand
+{
+    /// <summary>
+    /// Команда - скопировать данные в буфер обмена
+    /// </summary>
+    public class Command_Copy : Command
+    {
+        protected override bool CanExecute(object p)
+            => true;
+
+        protected override void Execute(object p)
+        {
+            App.GetService<IUserDialogService>()?
+                .SendMessageAsync("Команда", "Реализуйте команду: " + GetType().Name, App.Desktop.MainWindow);
+        }
+    }
+}
--- AvaloniaTemplate/Services/Registrations/RegistrationCommands.cs
+++ AvaloniaTemplate/Services/Registrations/RegistrationCommands.cs
@@ -19,8 +19,9 @@ namespace AvaloniaTemplate.Services.Registrations
             // 1. Создаем команды
             RegisterCommand<Command_Paste>("Command_Paste");
             RegisterCommand<Command_Cut>("Command_Cut");
+            RegisterCommand<Command_Copy>("Command_Copy");
 
-            //RegisterCommand<Command_Cut>("Command_Cut");
+            //RegisterCommand<Command_Copy>("Command_Copy");
 
             // 2. Регистрируем все типы команд в DI
             foreach (var kvp in commands)
